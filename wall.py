9bc99a76d43786e5d2cb741a78d661829a85ab7d
Gregg K. Steuben <steubeng@gmail.com>
2020-11-18 16:29:43 -0500

    wall is now 3 blocks thick of mossy cobblestone and 5 wide at the top with stone bricks

--- wall.py
+++ wall.py
@@ -1,11 +1,12 @@
 from mcpi.minecraft import Minecraft
 from mcpi.vec3 import Vec3
 from collections import deque
+from mcpi import block
 
 mc = Minecraft.create()
 mc.postToChat("Wall mode!!!")
 queue = deque([])
-itemsInQueuePerPosition = 3
+itemsInQueuePerPosition = 6
 minimumQueueSize = 2
 
 previousPos = mc.player.getTilePos()
@@ -13,10 +14,17 @@ while(True):
     currentPos = mc.player.getTilePos()
     if (previousPos.x != currentPos.x or previousPos.y != currentPos.y or previousPos.z != currentPos.z):
         print(currentPos)
+        queue.append(Vec3(currentPos.x+1, currentPos.y-3, currentPos.z+1)) # corer 1
+        queue.append(Vec3(currentPos.x-1, mc.getHeight(currentPos.x, currentPos.z)-3, currentPos.z-1)) # corner 2
+        queue.append(block.MOSS_STONE) # block id
         queue.append(Vec3(currentPos.x+2, currentPos.y-2, currentPos.z+2)) # corer 1
-        queue.append(Vec3(currentPos.x-2, mc.getHeight(currentPos.x, currentPos.z)-2, currentPos.z-2)) # corner 2
-        queue.append(45) # block id
+        queue.append(Vec3(currentPos.x-2, currentPos.y-2, currentPos.z-2)) # corner 2
+        queue.append(block.STONE_BRICK) # block id
         if (len(queue) > (itemsInQueuePerPosition * minimumQueueSize)):
+            corner1 = queue.popleft()
+            corner2 = queue.popleft()
+            blockId = queue.popleft()
+            mc.setBlocks(corner1.x, corner1.y, corner1.z, corner2.x, corner2.y, corner2.z, blockId)
             corner1 = queue.popleft()
             corner2 = queue.popleft()
             blockId = queue.popleft()
